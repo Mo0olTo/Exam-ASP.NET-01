3db1ef5ac122884a441a2a41185c30a951cd02e5
mo0olto <mostafa.allam.de@gmail.com>
2026-09-15 22:16:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam_ASP.NET_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
